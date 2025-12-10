dc0b2a1664f0ba4c170337227ff7c0e3d9934ac5
bovan494-wq <bovan494@gmail.com>
2025-12-10 10:51:38 +0200

    first commit

--- test.py
+++ test.py
@@ -1,46 +1,19 @@
-debit = merged_[merged_["TYPE"] == "DEBIT_SELF_ACQ"][["SEGMENT", "CLIENT_IDENTIFYCODE"]]
-credit = merged_[merged_["TYPE"] == "CREDIT_SELF_ACQ"][["SEGMENT", "CLIENT_IDENTIFYCODE"]]
-
-# групуємо по сегменту
-debit_grp = debit.groupby("SEGMENT")["CLIENT_IDENTIFYCODE"].apply(set)
-credit_grp = credit.groupby("SEGMENT")["CLIENT_IDENTIFYCODE"].apply(set)
-
-# перетин клієнтів у кожному сегменті
-duplicates_per_segment = {
-    seg: debit_grp.get(seg, set()) & credit_grp.get(seg, set())
-    for seg in merged_["SEGMENT"].unique()
-}
-
-dup_rows = []
-
-for seg in duplicates_per_segment:
-    dup_rows.append({
-        "SEGMENT": seg,
-        "clients_in_both_types": len(duplicates_per_segment[seg]),
-        "clients_only_debit": len(debit_grp.get(seg, set()) - credit_grp.get(seg, set())),
-        "clients_only_credit": len(credit_grp.get(seg, set()) - debit_grp.get(seg, set())),
-        "total_unique_clients": len(debit_grp.get(seg, set()) | credit_grp.get(seg, set()))
-    })
-
-duplicates_summary = pd.DataFrame(dup_rows)
-duplicates_summary
+debit_clients = set(merged_[merged_["TYPE"] == "DEBIT_SELF_ACQ"]["CLIENT_IDENTIFYCODE"])
+credit_clients = set(merged_[merged_["TYPE"] == "CREDIT_SELF_ACQ"]["CLIENT_IDENTIFYCODE"])
+duplicate_ids_df = pd.DataFrame({"CLIENT_IDENTIFYCODE": list(duplicate_ids)})
+duplicate_ids_df
 
 
-duplicate_clients_table = []
-
-for seg in duplicates_per_segment:
-    for client in duplicates_per_segment[seg]:
-        duplicate_clients_table.append({
-            "SEGMENT": seg,
-            "CLIENT_IDENTIFYCODE": client
-        })
-
-duplicate_clients_table = pd.DataFrame(duplicate_clients_table)
-duplicate_clients_table
-
-
-segment_stats["clients_in_both_types"] = segment_stats["SEGMENT"].map(
-    lambda seg: len(duplicates_per_segment.get(seg, set()))
+dups_per_segment = (
+    merged_[merged_["CLIENT_IDENTIFYCODE"].isin(duplicate_ids)]
+    .groupby("SEGMENT")["CLIENT_IDENTIFYCODE"]
+    .unique()
+    .reset_index()
 )
 
-segment_stats
+dups_per_segment
+
+
+duplicates_full = merged_[merged_["CLIENT_IDENTIFYCODE"].isin(duplicate_ids)]
+duplicates_full.sort_values(["CLIENT_IDENTIFYCODE", "TYPE"])
+
