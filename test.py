55a94a164b953530efc0d0e1ebd262b894b86526
bovan494-wq <bovan494@gmail.com>
2025-12-15 14:16:43 +0200

    first commit

--- test.py
+++ test.py
@@ -1,6 +1,20 @@
-df["TYPE_NEW"] = np.where(
-    df["CONTRAGENTAIDENTIFYCODE"].astype(str)
-        == df["CONTRAGENTBIDENTIFYCODE"].astype(str),
-    "CREDIT_SELF_ACQ",
-    "DEBIT_SELF_ACQ"
+STATUS_MAP = {
+    "Зареєстровано": "ACTIVE",
+
+    "В стані припинення": "SUSPENDED",
+
+    "Припинено": "TERMINATED",
+    "Скасовано": "TERMINATED",
+    "Архівний": "ARCHIVED",
+
+    "Порушено справу про банкрутство": "BANKRUPTCY",
+    "Порушено справу про банкрутство (санація)": "BANKRUPTCY",
+
+    "Зареєстровано, свідоцтво про державну реєстрацію недійсне": "INVALID_REGISTRATION",
+}
+
+temp["status_std"] = (
+    temp["Статус"]
+    .map(STATUS_MAP)
+    .fillna("OTHER")
 )
