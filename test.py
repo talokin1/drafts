136f02ced49ed988cc6eea39ec7712ef7d1a0b09
bovan494-wq <bovan494@gmail.com>
2025-12-19 12:08:10 +0200

    first commit

--- test.py
+++ test.py
@@ -1,26 +1,41 @@
-def detect_credit_acquiring(df, period):
-    df = df.copy()
+import pandas as pd
+from pathlib import Path
 
-    is_credit = df["BANKBID"] == MFO_OTP
-    is_not_otp_sender = df["BANKAID"] != MFO_OTP
-    is_acq_text = is_acquiring_only(df["PLATPURPOSE"])
+files = [
+    r"M:\Controlling\Data_Science_Projects\Corp_Churn\Data\Raw\data_trxs_2025_02.parquet",
+    r"M:\Controlling\Data_Science_Projects\Corp_Churn\Data\Raw\data_trxs_2025_03.parquet",
+    r"M:\Controlling\Data_Science_Projects\Corp_Churn\Data\Raw\data_trxs_2025_04.parquet",
+    r"M:\Controlling\Data_Science_Projects\Corp_Churn\Data\Raw\data_trxs_2025_05.parquet",
+    r"M:\Controlling\Data_Science_Projects\Corp_Churn\Data\Raw\data_trxs_2025_06.parquet",
+    r"M:\Controlling\Data_Science_Projects\Corp_Churn\Data\Raw\data_trxs_2025_07.parquet",
+    r"M:\Controlling\Data_Science_Projects\Corp_Churn\Data\Raw\data_trxs_2025_08.parquet",
+    r"M:\Controlling\Data_Science_Projects\Corp_Churn\Data\Raw\data_trxs_2025_09.parquet",
+    r"M:\Controlling\Data_Science_Projects\Corp_Churn\Data\Raw\data_trxs_2025_10.parquet",
+    r"M:\Controlling\Data_Science_Projects\Corp_Churn\Data\Raw\data_trxs_2025_11.parquet",
+]
 
-    result = df[
-        is_credit &
-        is_not_otp_sender &
-        is_acq_text
-    ].copy()
+output_path = r"M:\Controlling\Data_Science_Projects\Corp_Churn\Results\2924_flows.xlsx"
 
-    # базово беремо A
-    result["CLIENT_EDRPOU"] = result["CONTRAGENTAIDENTIFYCODE"].astype(str)
+with pd.ExcelWriter(output_path, engine="xlsxwriter") as writer:
+    for file in files:
+        df = pd.read_parquet(file)
 
-    # якщо A — банк, то клієнт у B
-    mask_bank_edrpou = result["CLIENT_EDRPOU"].isin(BANK_EDRPOU)
+        df["ACCOUNTANO"] = df["ACCOUNTANO"].astype(str)
+        df["ACCOUNTBNO"] = df["ACCOUNTBNO"].astype(str)
 
-    result.loc[mask_bank_edrpou, "CLIENT_EDRPOU"] = (
-        result.loc[mask_bank_edrpou, "CONTRAGENTBIDENTIFYCODE"].astype(str)
-    )
+        mask = (
+            (df["BANKAID"] != 300528) &
+            (df["ACCOUNTANO"].str.startswith("2924")) &
+            (
+                df["ACCOUNTBNO"].str.startswith("2600") |
+                df["ACCOUNTBNO"].str.startswith("2650") |
+                df["ACCOUNTBNO"].str.startswith("2655")
+            )
+        )
 
-    result["PERIOD"] = period
+        df_filtered = df[mask]
 
-    return result.drop_duplicates()
+        month = Path(file).stem.replace("data_trxs_", "")  # 2025_02
+        sheet_name = month.replace("_", "-")               # 2025-02
+
+        df_filtered.to_excel(writer, sheet_name=sheet_name, index=False)
