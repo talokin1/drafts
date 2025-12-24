20f2b20a0bc300dafcf29d0d9034a0b5d90369df
bovan494-wq <bovan494@gmail.com>
2025-12-25 00:04:43 +0200

    first commit

--- test.py
+++ test.py
@@ -1,16 +1,44 @@
-RE_REFUND = re.compile(
+RE_CASH_REFUND = re.compile(
     r"""
-    \bвідшк\w*\b |
-    \brefund\b |
-    \breversal\b
+    (відшкод|повернен|reversal|refund)
+    .*?
+    (видач\w*\s*готів|готівк)
     """,
     re.IGNORECASE | re.VERBOSE
 )
 
-# === STRONG INCLUDE: any refund is acquiring ===
-if RE_REFUND.search(pp):
+RE_SETTLEMENT_ACQ = re.compile(
+    r"""
+    (розрах\w*|розрахунок)
+    .*?
+    (тт|merchant|tt\s*[a-z0-9_]+)
+    """,
+    re.IGNORECASE | re.VERBOSE
+)
+
+if RE_SETTLEMENT_ACQ.search(pp):
+    reasons.append("pp_settlement_acquiring")
+    pp_hit = True
+
+    RE_INSTALLMENTS = re.compile(
+    r"(оплата\s*частинами|покупк\w*\s*частинами|split\s*pay)",
+    re.IGNORECASE
+)
+
+
+# NEGATIVE: cash refund is NOT acquiring
+if RE_CASH_REFUND.search(pp):
     return pd.Series({
-        "is_acquiring": True,
-        "acq_reason": "refund",
-        "acq_score": 1
+        "is_acquiring": False,
+        "acq_reason": "cash_refund",
+        "acq_score": 0
     })
+
+# NEGATIVE: installments are NOT acquiring
+if RE_INSTALLMENTS.search(pp):
+    return pd.Series({
+        "is_acquiring": False,
+        "acq_reason": "installments",
+        "acq_score": 0
+    })
+
