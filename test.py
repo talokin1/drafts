de0c2b771b70bec6ecb64f450a624863e2a594df
bovan494-wq <bovan494@gmail.com>
2025-12-25 00:51:43 +0200

    first commit

--- test.py
+++ test.py
@@ -1,27 +1,38 @@
-RE_BANK_CP = re.compile(
+RE_HOUSEHOLD = re.compile(
     r"""
-    \bбанк\b |                      # будь-який "банк"
-    raiffeisen | райффайзен | райф | # Райффайзен
-    ощад | oschad |                 # Ощад
-    сенс | sense |                  # Sense
-    приват | privat |               # Приват
-    укрсиб | ukrsib |               # Укрсиб
-    укргаз | ukrgas |               # Укргаз
-    пумб | pumb |                   # PUMB
-    mono | monobank |               # mono
-    otp\b | otp\s*bank              # OTP
+    житлово[\-\s]?комунальн |
+    комун\w* |
+    ком\.послуг |
+    рахунок\s+за\s+сплат |
+    оренд\w* |
+    паркомісц |
+    платник\b |
+    \bіпн\b
+    """,
+    re.IGNORECASE | re.VERBOSE
+)
+
+RE_ACQ_MARKERS = re.compile(
+    r"""
+    \bекв\b|еквайр\w*|acquir\w*|
+    liqpay|split\s*id|type\s*acquir|
+    відшк\w*|покрит\w*
+    """,
+    re.IGNORECASE | re.VERBOSE
+)
+
+RE_BANK_LIKE = re.compile(
+    r"""
+    \b(ат|ао|пат|прат|jsc|pjsc)\b      # організаційна форма (може бути)
+    .*?                               # будь-що між
+    \bбанк\b                          # слово банк
+    |
+    \bбанк\b.*?\b(ат|ао|пат|прат|jsc|pjsc)\b
     """,
     re.IGNORECASE | re.VERBOSE
 )
 
 
-# HARD STOP: household payments to BANK are NOT acquiring
-if RE_HOUSEHOLD_NEG.search(pp) and RE_BANK_CP.search(cp):
-    # виняток: якщо є явний acquiring-сигнал у cp/pp (liqpay/type acquiring/екв)
-    if not any([
-        RE_TYPE_ACQ.search(pp) or RE_TYPE_ACQ.search(cp),
-        RE_INTERNET_ACQ_CP.search(cp),
-        re.search(r"\bекв\b|еквайр|acquir|liqpay|split\s*id|type\s*acquir", cp, re.IGNORECASE),
-        re.search(r"\bекв\b|еквайр|acquir|liqpay|split\s*id|type\s*acquir", pp, re.IGNORECASE),
-    ]):
+
+if RE_HOUSEHOLD.search(pp) and RE_BANK_LIKE.search(cp) and not RE_ACQ_MARKERS.search(text):
         return pd.Series({"is_acquiring": False, "acq_reason": "", "acq_score": 0})
