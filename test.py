950b86ad57cbcf24a9ab705f0a50130f641d24e4
bovan494-wq <bovan494@gmail.com>
2025-12-25 01:27:50 +0200

    first commit

--- test.py
+++ test.py
@@ -1,38 +1,14 @@
-RE_HOUSEHOLD = re.compile(
-    r"""
-    житлово[\-\s]?комунальн |
-    комун\w* |
-    ком\.послуг |
-    рахунок\s+за\s+сплат |
-    оренд\w* |
-    паркомісц |
-    платник\b |
-    \bіпн\b
-    """,
-    re.IGNORECASE | re.VERBOSE
+df_acq = df[df["is_acquiring"]].copy()
+agg_acq = (
+    df_acq
+    .groupby("CONTRAGENTBIDENTIFYCODE")
+    .agg(
+        acq_turnover=("SUMMAEQ", "sum"),
+        acq_txn_cnt=("SUMMAEQ", "size"),
+        sample_platpurpose=("PLATPURPOSE", lambda x: x.dropna().iloc[0] if len(x.dropna()) else ""),
+        sample_counterparty=("CONTRAGENTASNAME", lambda x: x.dropna().iloc[0] if len(x.dropna()) else ""),
+        reasons=("acq_reason", lambda x: " | ".join(sorted(set(x))))
+    )
+    .reset_index()
+    .sort_values("acq_turnover", ascending=False)
 )
-
-RE_ACQ_MARKERS = re.compile(
-    r"""
-    \bекв\b|еквайр\w*|acquir\w*|
-    liqpay|split\s*id|type\s*acquir|
-    відшк\w*|покрит\w*
-    """,
-    re.IGNORECASE | re.VERBOSE
-)
-
-RE_BANK_LIKE = re.compile(
-    r"""
-    \b(ат|ао|пат|прат|jsc|pjsc)\b      # організаційна форма (може бути)
-    .*?                               # будь-що між
-    \bбанк\b                          # слово банк
-    |
-    \bбанк\b.*?\b(ат|ао|пат|прат|jsc|pjsc)\b
-    """,
-    re.IGNORECASE | re.VERBOSE
-)
-
-
-
-if RE_HOUSEHOLD.search(pp) and RE_BANK_LIKE.search(cp) and not RE_ACQ_MARKERS.search(text):
-        return pd.Series({"is_acquiring": False, "acq_reason": "", "acq_score": 0})
