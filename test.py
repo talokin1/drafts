46559a4f70d9a65385eff52c038e9591b2c5dbe1
bovan494-wq <bovan494@gmail.com>
2025-12-28 14:24:00 +0200

    first commit

--- test.py
+++ test.py
@@ -1,13 +1,45 @@
-# 1. ЄДРПОУ з clients
-clients_set = set(clients["IDENTIFYCODE"].astype(str))
+artifact = {
+    # ---------- Primary ----------
+    "primary": {
+        "model": lgc_primary,
+        "features": features_primary,
+        "threshold": 0.5
+    },
 
-# 2. ЄДРПОУ, у яких реально є еквайринг у df
-acq_set = set(
-    df.loc[df["Ознака екв"] == "екв", "CONTRAGENTBIDENTIFYCODE"]
-      .astype(str)
+    "income_bins": {
+        "model": multiclass_model,
+        "features": features_multiclass,
+        "label_encoder": label_encoder,
+        "bin_edges": bin_edges,
+        "bin_labels": bin_labels
+    }
+}
+
+import pickle
+
+with open("corp_liabilities_pipeline.pkl", "wb") as f:
+    pickle.dump(artifact, f)
+
+
+
+
+
+with open("corp_liabilities_pipeline.pkl", "rb") as f:
+    artifact = pickle.load(f)
+X_primary = main_dataset[artifact["primary"]["features"]]
+
+main_dataset["PRIMARY_SCORE"] = (
+    artifact["primary"]["model"]
+    .predict_proba(X_primary)[:, 1]
 )
 
-# 3. Клієнти без еквайрингу, але які потрапили в clients
-clients_without_acq = clients_set - acq_set
+X_bins = main_dataset[artifact["income_bins"]["features"]]
 
-len(clients_without_acq)
+proba_bins = artifact["income_bins"]["model"].predict_proba(X_bins)
+
+bins_df = pd.DataFrame(
+    proba_bins,
+    columns=artifact["income_bins"]["label_encoder"].classes_
+)
+
+main_dataset = pd.concat([main_dataset, bins_df], axis=1)
