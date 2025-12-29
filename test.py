1fbac4b90b2046d409d6bb42db157a61f3170e4b
bovan494-wq <bovan494@gmail.com>
2025-12-29 18:26:16 +0200

    first commit

--- test.py
+++ test.py
@@ -1,87 +1,88 @@
+import lightgbm as lgb
+from lightgbm import LGBMRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import numpy as np
 
-def fit_numeric_stats(X_train, clip_low=0.01, clip_high=0.99):
-    stats = {}
+lgb_reg = LGBMRegressor(
+    boosting_type="gbdt",
+    objective="regression",
+    learning_rate=0.02,
+    n_estimators=3000,
+    num_leaves=41,
+    max_depth=30,
+    min_child_samples=20,
+    min_child_weight=0.01,
+    min_split_gain=0.0,
+    subsample=1.0,
+    colsample_bytree=1.0,
+    random_state=100,
+    n_jobs=-1,
+    importance_type="gain"
+)
 
-    num_cols = X_train.select_dtypes(include=np.number).columns
-
-    for c in num_cols:
-        s = X_train[c]
-        stats[c] = {
-            "median": s.median(),
-            "clip_low": s.quantile(clip_low),
-            "clip_high": s.quantile(clip_high)
-        }
-
-    return stats
-
-
-def apply_numeric_preprocess(X, stats):
-    X = X.copy()
-
-    for c, p in stats.items():
-        if c not in X.columns:
-            continue
-
-        X[c] = X[c].fillna(p["median"])
-        X[c] = X[c].clip(p["clip_low"], p["clip_high"])
-
-        if c.endswith("_DIF"):
-            X[c] = np.sign(X[c]) * np.log1p(np.abs(X[c]))
-
-    return X
+lgb_reg.fit(
+    X_train_proc,
+    y_train,
+    eval_set=[(X_valid_proc, y_valid)],
+    eval_metric="mae",
+    callbacks=[lgb.early_stopping(stopping_rounds=50, verbose=True)]
+)
 
 
-def apply_numeric_preprocess(X, stats):
-    X = X.copy()
+features = X_train_proc.columns
+importances = lgb_reg.feature_importances_
 
-    for c, p in stats.items():
-        if c not in X.columns:
-            continue
+feature_importance = (
+    pd.DataFrame({
+        "features": features,
+        "importance": importances
+    })
+    .sort_values("importance", ascending=False)
+    .reset_index(drop=True)
+)
 
-        X[c] = X[c].fillna(p["median"])
-        X[c] = X[c].clip(p["clip_low"], p["clip_high"])
-
-        if c.endswith("_DIF"):
-            X[c] = np.sign(X[c]) * np.log1p(np.abs(X[c]))
-
-    return X
-
-def fit_categories(X_train):
-    cat_info = {}
-
-    cat_cols = X_train.select_dtypes(include="object").columns
-
-    for c in cat_cols:
-        cat_info[c] = set(
-            X_train[c].fillna("unknown").astype(str).unique()
-        )
-
-    return cat_info
+feature_importance.head(20)
 
 
+train_pred = lgb_reg.predict(X_train_proc)
 
-def apply_categories(X, cat_info):
-    X = X.copy()
+X_train_total = pd.concat(
+    [
+        X_train.reset_index(drop=True),
+        y_train.reset_index(drop=True),
+        pd.Series(train_pred, name="MODEL_PRED")
+    ],
+    axis=1
+)
 
-    for c, allowed in cat_info.items():
-        if c not in X.columns:
-            continue
+mae_train = mean_absolute_error(y_train, train_pred)
+rmse_train = mean_squared_error(y_train, train_pred, squared=False)
+r2_train = r2_score(y_train, train_pred)
 
-        X[c] = X[c].fillna("unknown").astype(str)
-        X[c] = X[c].where(X[c].isin(allowed), "unknown")
-        X[c] = X[c].astype("category")
+print("TRAIN METRICS")
+print("MAE:", round(mae_train, 2))
+print("RMSE:", round(rmse_train, 2))
+print("R2:", round(r2_train, 4))
 
-    return X
 
 
-# FIT тільки на train
-num_stats = fit_numeric_stats(X_train)
-cat_info = fit_categories(X_train)
 
-# APPLY
-X_train_proc = apply_numeric_preprocess(X_train, num_stats)
-X_train_proc = apply_categories(X_train_proc, cat_info)
+valid_pred = lgb_reg.predict(X_valid_proc)
 
-X_valid_proc = apply_numeric_preprocess(X_valid, num_stats)
-X_valid_proc = apply_categories(X_valid_proc, cat_info)
+X_valid_total = pd.concat(
+    [
+        X_valid.reset_index(drop=True),
+        y_valid.reset_index(drop=True),
+        pd.Series(valid_pred, name="MODEL_PRED")
+    ],
+    axis=1
+)
+
+mae_valid = mean_absolute_error(y_valid, valid_pred)
+rmse_valid = mean_squared_error(y_valid, valid_pred, squared=False)
+r2_valid = r2_score(y_valid, valid_pred)
+
+print("VALID METRICS")
+print("MAE:", round(mae_valid, 2))
+print("RMSE:", round(rmse_valid, 2))
+print("R2:", round(r2_valid, 4))
