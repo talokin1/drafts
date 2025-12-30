d95f416dc68317a833e9a947b71ad2cf1bc62639
bovan494-wq <bovan494@gmail.com>
2025-12-30 14:53:39 +0200

    first commit

--- test.py
+++ test.py
@@ -1,20 +1,88 @@
-LGBMRegressor(
-    boosting_type="gbdt",
-    objective="regression",
-    learning_rate=0.03,
-    n_estimators=2000,
+# назви колонок
+TARGET_COL = y_train.name if hasattr(y_train, "name") else "TARGET"
+PRED_COL = "MODEL_PRED"
 
-    max_depth=8,
-    num_leaves=31,
+# train
+x_train_total = x_train_total.rename(columns={x_train_total.columns[-2]: TARGET_COL})
 
-    min_child_samples=50,
-    min_child_weight=0.1,
-    min_split_gain=0.01,
+# valid
+x_valid_total = x_valid_total.rename(columns={x_valid_total.columns[-2]: TARGET_COL})
 
-    subsample=0.8,
-    colsample_bytree=0.8,
 
-    random_state=100,
-    n_jobs=-1,
-    importance_type="gain"
+x_train_total.to_csv(
+    "regression_train_results.csv",
+    index=False,
+    encoding="utf-8"
+)
+
+x_valid_total.to_csv(
+    "regression_valid_results.csv",
+    index=False,
+    encoding="utf-8"
+)
+
+
+BINS = [0, 5_000, 10_000, 20_000, 30_000, 50_000, np.inf]
+BIN_LABELS = [
+    "0-5k",
+    "5-10k",
+    "10-20k",
+    "20-30k",
+    "30-50k",
+    "50k+"
+]
+
+
+x_train_total["INCOME_BIN"] = pd.cut(
+    x_train_total[TARGET_COL],
+    bins=BINS,
+    labels=BIN_LABELS,
+    right=False
+)
+
+x_valid_total["INCOME_BIN"] = pd.cut(
+    x_valid_total[TARGET_COL],
+    bins=BINS,
+    labels=BIN_LABELS,
+    right=False
+)
+
+
+x_train_total["PRED_BIN"] = pd.cut(
+    x_train_total[PRED_COL],
+    bins=BINS,
+    labels=BIN_LABELS,
+    right=False
+)
+
+x_valid_total["PRED_BIN"] = pd.cut(
+    x_valid_total[PRED_COL],
+    bins=BINS,
+    labels=BIN_LABELS,
+    right=False
+)
+
+
+pd.crosstab(
+    x_valid_total["INCOME_BIN"],
+    x_valid_total["PRED_BIN"],
+    normalize="index"
+)
+
+
+
+
+
+
+
+x_train_total.to_csv(
+    "regression_train_with_bins.csv",
+    index=False,
+    encoding="utf-8"
+)
+
+x_valid_total.to_csv(
+    "regression_valid_with_bins.csv",
+    index=False,
+    encoding="utf-8"
 )
