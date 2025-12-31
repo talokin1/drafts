37548c858b2de214378480620e7fb49eea8de01e
bovan494-wq <bovan494@gmail.com>
2025-12-31 10:48:15 +0200

    first commit

--- test.py
+++ test.py
@@ -1,5 +1,31 @@
+clients_ids = clients["CONTRAGENTID"].astype(int).tolist()
+
+dates_sql = [
+    f"date '{pd.to_datetime(d, dayfirst=True).date()}'"
+    for d in dates
+]
+
+
+QUERY = f"""
 select
+    contragentid,
+    arcdate,
     max(balance_amt_uah) as max_balance_amt_uah
 from b2_olap.ar_deals@dwh
-where contragentid = 3050596
-  and arcdate = date '2025-11-30';
+where contragentid in ({','.join(map(str, clients_ids))})
+  and arcdate in ({','.join(dates_sql)})
+group by contragentid, arcdate
+"""
+
+df = get_data(QUERY)
+result = (
+    df
+    .pivot(
+        index="CONTRAGENTID",
+        columns="ARCDATE",
+        values="MAX_BALANCE_AMT_UAH"
+    )
+    .fillna(0)
+    .sort_index()
+)
+result.columns = result.columns.strftime("%d.%m.%Y")
