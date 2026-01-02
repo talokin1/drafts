b858938937169311001b8e7c106a4fb9857be1a5
bovan494-wq <bovan494@gmail.com>
2026-01-02 13:06:45 +0200

    first commit

--- test.py
+++ test.py
@@ -1,33 +1,11 @@
-results = []
-MAX_IN = 900  # запас від 1000
-
-for arcdate, group in data.groupby("Дата проведення"):
-    ids = (
-        group["Ідентифікатор заявки в АБС"]
-        .dropna()
-        .astype(int)
-        .unique()
-    )
-
-    if len(ids) == 0:
-        continue
-
-    for i in range(0, len(ids), MAX_IN):
-        sub_ids = ids[i:i + MAX_IN]
-        ids_str = ",".join(map(str, sub_ids))
-
-        sql = f"""
-        SELECT
-            arcdate,
-            id,
-            bankaID,
-            bankbID
-        FROM b2_olap.ar_document@dwh
-        WHERE arcdate = TO_DATE('{arcdate.strftime("%d.%m.%Y")}', 'DD.MM.YYYY')
-          AND id IN ({ids_str})
-        """
-
-        df_part = get_data(sql)
-        results.append(df_part)
-
-    print(f"{arcdate.date()} → {len(ids)} IDs")
+sql = f"""
+SELECT
+    arcdate,
+    id,
+    bankaID,
+    bankbID
+FROM b2_olap.ar_document@dwh
+WHERE arcdate >= TO_DATE('{arcdate.strftime("%d.%m.%Y")}', 'DD.MM.YYYY')
+  AND arcdate <  TO_DATE('{(arcdate + pd.Timedelta(days=1)).strftime("%d.%m.%Y")}', 'DD.MM.YYYY')
+  AND id IN ({ids_str})
+"""
