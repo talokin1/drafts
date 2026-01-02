f07b3b486c3f8098c486cde3312c99a179ef90d7
bovan494-wq <bovan494@gmail.com>
2026-01-02 13:00:41 +0200

    first commit

--- test.py
+++ test.py
@@ -1,46 +1,33 @@
-
-data["Дата проведення"] = pd.to_datetime(
-    data["Дата проведення"],
-    dayfirst=True
-)
-data = data.sort_values("Дата проведення")
-
-
-
-
 results = []
+MAX_IN = 900  # запас від 1000
 
 for arcdate, group in data.groupby("Дата проведення"):
-    ids = group["Ідентифікатор заявки в АБС"].dropna().unique()
+    ids = (
+        group["Ідентифікатор заявки в АБС"]
+        .dropna()
+        .astype(int)
+        .unique()
+    )
 
     if len(ids) == 0:
         continue
 
+    for i in range(0, len(ids), MAX_IN):
+        sub_ids = ids[i:i + MAX_IN]
+        ids_str = ",".join(map(str, sub_ids))
 
-    ids_str = ",".join(str(int(x)) for x in ids)
+        sql = f"""
+        SELECT
+            arcdate,
+            id,
+            bankaID,
+            bankbID
+        FROM b2_olap.ar_document@dwh
+        WHERE arcdate = TO_DATE('{arcdate.strftime("%d.%m.%Y")}', 'DD.MM.YYYY')
+          AND id IN ({ids_str})
+        """
 
-    sql = f"""
-    SELECT
-        arcdate,
-        id,
-        bankaID,
-        bankbID
-    FROM b2_olap.ar_document@dwh
-    WHERE arcdate = TO_DATE('{arcdate.strftime("%d.%m.%Y")}', 'DD.MM.YYYY')
-      AND id IN ({ids_str})
-    """
-
-    df_part = get_data(sql)
-    results.append(df_part)
+        df_part = get_data(sql)
+        results.append(df_part)
 
     print(f"{arcdate.date()} → {len(ids)} IDs")
-
-bank_df = pd.concat(results, ignore_index=True)
-
-
-data = data.merge(
-    bank_df,
-    left_on=["Дата проведення", "Ідентифікатор заявки в АБС"],
-    right_on=["arcdate", "id"],
-    how="left"
-)
