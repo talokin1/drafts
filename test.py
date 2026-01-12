655dad322530488091a322c62add701e24163088
bovan494-wq <bovan494@gmail.com>
2026-01-12 11:20:49 +0200

    first commit

--- test.py
+++ test.py
@@ -1,7 +1,34 @@
-raw_numbers = (
-    df
-    .apply(pd.to_numeric, errors='coerce')  # усе → float або NaN
-    .stack()                                # в один Series
-    .dropna()                               # прибираємо NaN
-    .tolist()
+opf_ref = (
+    opf_df[["FIRM_OPFCD", "FIRM_OPFNM"]]
+    .dropna()
+    .drop_duplicates()
+    .rename(columns={
+        "FIRM_OPFCD": "OPF_CODE",
+        "FIRM_OPFNM": "OPF_NAME"
+    })
+)
+
+opf_ref["OPF_NAME_NORM"] = (
+    opf_ref["OPF_NAME"]
+    .str.lower()
+    .str.replace(r"\s+", " ", regex=True)
+    .str.strip()
+)
+
+df["FULL_NAME_NORM"] = (
+    df["Повна назва"]
+    .astype(str)
+    .str.lower()
+    .str.replace("[«»\"']", "", regex=True)
+    .str.replace(r"\s+", " ", regex=True)
+    .str.strip()
+)
+
+def extract_opf(full_name_norm, opf_ref):
+    for _, r in opf_ref.iterrows():
+        if full_name_norm.startswith(r["OPF_NAME_NORM"]):
+            return r["OPF_CODE"], r["OPF_NAME"]
+    return None, None
+df[["OPF_CODE", "OPF_NAME"]] = df["FULL_NAME_NORM"].apply(
+    lambda x: pd.Series(extract_opf(x, opf_ref))
 )
