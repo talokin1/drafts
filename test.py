f32646d7482f8e8fe64b08a15029857bec044f7a
bovan494-wq <bovan494@gmail.com>
2026-01-12 18:00:51 +0200

    first commit

--- test.py
+++ test.py
@@ -1,9 +1,27 @@
-RE_OPERATIONAL_REFUND = re.compile(
-    r"(відшкод\w*|refund).*(коміс|fee|corr|adjust)",
-    re.IGNORECASE
-)
+is_refund_cash = RE_REFUND_CASH_STRICT.search(full_text)
+is_refund_acq  = RE_REFUND_ACQ.search(full_text)
 
-if RE_OPERATIONAL_REFUND.search(full_text):
+
+if is_refund_cash:
+    return {
+        "is_acquiring": True,
+        "reason": "refund_cash_acquiring",
+        "score": 3
+    }
+
+if is_refund_acq:
+    return {
+        "is_acquiring": True,
+        "reason": "refund_acquiring",
+        "score": 2
+    }
+
+
+
+if (
+    RE_OPERATIONAL_REFUND.search(full_text)
+    and not (is_refund_cash or is_refund_acq)
+):
     return {
         "is_acquiring": False,
         "reason": "operational_refund",
