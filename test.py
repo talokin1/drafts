d0bce3cdb3143c22b1eac49b0c38979eea1ddab8
bovan494-wq <bovan494@gmail.com>
2026-01-12 13:30:00 +0200

    first commit

--- test.py
+++ test.py
@@ -1,20 +1,11 @@
-month_map = {
-    "Січень": "01", "Лютий": "02", "Березень": "03", "Квітень": "04",
-    "Травень": "05", "Червень": "06", "Липень": "07", "Серпень": "08",
-    "Вересень": "09", "Жовтень": "10", "Листопад": "11", "Грудень": "12"
-}
+def normalize_kved(x):
+    if not isinstance(x, str):
+        return None
+    x = x.strip()
 
-tmp = df["Дата дзвінка"].str.split(", ", expand=True)
+    # 91.31.0 → 91.31
+    if x.count(".") >= 2:
+        x = ".".join(x.split(".")[:2])
 
-df["month_name"] = tmp[0]
-df["year"] = tmp[1]
-df["month_num"] = df["month_name"].map(month_map)
-
-df["year_month"] = df["year"] + "_" + df["month_num"]
-
-for ym, g in df.groupby("year_month"):
-    filename = f"NPS_{ym}.parquet"
-    g.to_parquet(filename, index=False)
-    print(f"Saved {filename} | Rows = {len(g)}")
-
-df.groupby("year_month").size().sum() == len(df)
+    return x
+df["KVED_NORM"] = df["KVED"].apply(normalize_kved)
