db7d81f5a2fe17fe81f6a2be4612acc930e625ed
bovan494-wq <bovan494@gmail.com>
2026-01-12 18:29:51 +0200

    first commit

--- test.py
+++ test.py
@@ -8,3 +8,11 @@ RE_MERCHANT_CASH = re.compile(
     """,
     re.IGNORECASE | re.VERBOSE
 )
+
+if RE_MERCHANT_CASH.search(pp_text):
+    return pd.Series({
+        "is_acquiring": True,
+        "acq_reason": "merchant_cash_withdrawal",
+        "acq_score": 3,
+    })
+
