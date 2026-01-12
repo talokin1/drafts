1f304f9e04d44d0ab15eb67c96cc19d458f9641c
bovan494-wq <bovan494@gmail.com>
2026-01-12 16:34:26 +0200

    first commit

--- test.py
+++ test.py
@@ -1,21 +1,13 @@
-RE_REFUND_ACQ = re.compile(
-    r"(відшк\w*|refund).*(екв|acquir|liqpay)",
-    re.IGNORECASE
-)
+if RE_REFUND_ACQ.search(pp_text):
+    return pd.Series({
+        "is_acquiring": True,
+        "acq_reason": "refund_acquiring",
+        "acq_score": 1,
+    })
 
-RE_REFUND_CASH = re.compile(
-    r"(відшк\w*|refund).*(видач\w*.*?(готів|гот\.?)|cash)",
-    re.IGNORECASE
-)
-
-
-
-if RE_INTERNET_ACQ_CP.search(cp) or RE_COUNTERPARTY.search(cp):
-        return pd.Series({"is_acquiring": True, "acq_reason": "cp_strong", "acq_score": 1})
-
-# 2) Purpose strong: refund (включає cash refund)
-if RE_REFUND_ACQ.search(pp):
-    return pd.Series({"is_acquiring": True, "acq_reason": "refund_acq", "acq_score": 1})
-
-if RE_REFUND_CASH.search(pp):
-    return pd.Series({"is_acquiring": True, "acq_reason": "refund_cash", "acq_score": 1})
+if RE_REFUND_CASH.search(pp_text):
+    return pd.Series({
+        "is_acquiring": True,
+        "acq_reason": "refund_cash",
+        "acq_score": 1,
+    })
