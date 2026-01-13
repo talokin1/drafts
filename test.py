86f6152b40598234c6829faec8b5a98a04769d9a
bovan494-wq <bovan494@gmail.com>
2026-01-13 10:06:16 +0200

    first commit

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-\bтранзит\w*\b.*\b(mpos|pos)\b|\b(mpos|pos)\b.*\bтранзит\w*\b
-if RE_ACQ_TRANSIT.search(text):
-    return True
-
+RE_ACQ_TRANSIT = re.compile(
+    r"\b(транзит\w*.*(mpos|pos)|(mpos|pos).*\bтранзит\w*)\b",
+    re.IGNORECASE
+)
