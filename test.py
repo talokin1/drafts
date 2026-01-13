01f81ec4f71b925e5c7782c438c51a8354d5a2ed
bovan494-wq <bovan494@gmail.com>
2026-01-13 18:37:39 +0200

    first commit

--- test.py
+++ test.py
@@ -1,29 +1,25 @@
-RE_REFUND_NON_ACQ = re.compile(
+RE_REFUND_UTILITIES = re.compile(
     r"\b("
-    r"подат(ок|ку|ки)|"
-    r"земел\w*|"
-    r"жкг|"
-    r"водопостачан\w*|"
     r"електроенерг\w*|"
+    r"ел\.?енерг\w*|"
+    r"е/енерг\w*|"
+    r"теплопостачан\w*|"
+    r"водопостачан\w*|"
+    r"реактивн\w*|"
     r"газопостачан\w*|"
-    r"утриманн\w*|"
-    r"комунальн\w*|"
-    r"оренд\w*"
+    r"вивезенн\w*|"
+    r"тпв|"
+    r"жкг"
     r")\b",
     re.IGNORECASE
 )
 
-RE_REFUND_ACQ = re.compile(
-    r"\b("
-    r"еквайр\w*|"
-    r"торговельн\w*|"
-    r"merchant|"
-    r"cmps|"
-    r"kasa|"
-    r"pos|"
-    r"термінал\w*|"
-    r"ком\s*бан|"
-    r"коміс\w*"
-    r")\b",
-    re.IGNORECASE
-)
+text = normalize_ua(pp_text)
+
+if RE_REFUND_BASE.search(text) and (
+    RE_REFUND_NON_ACQ_STRONG.search(text)
+    or RE_REFUND_UTILITIES.search(text)
+    or RE_REFUND_TAX.search(text)
+):
+    is_acquiring = False
+    reason = "refund_non_acquiring"
