9d0fb28ebd8c0179e69d128034611f314f189729
bovan494-wq <bovan494@gmail.com>
2026-01-13 10:50:38 +0200

    first commit

--- test.py
+++ test.py
@@ -1,4 +1,13 @@
-RE_ACQ_TRANSIT = re.compile(
-    r"\b(транзит\w*.*(mpos|pos)|(mpos|pos).*\bтранзит\w*)\b",
-    re.IGNORECASE
-)
+def normalize_ua(text: str) -> str:
+    table = str.maketrans({
+        "A": "А", "B": "В", "C": "С", "E": "Е", "H": "Н",
+        "I": "І", "K": "К", "M": "М", "O": "О", "P": "Р",
+        "T": "Т", "X": "Х",
+        "a": "а", "b": "в", "c": "с", "e": "е", "h": "н",
+        "i": "і", "k": "к", "m": "м", "o": "о", "p": "р",
+        "t": "т", "x": "х",
+    })
+    return text.translate(table)
+
+test_norm = normalize_ua(test)
+re.search(r"відшк", test_norm, re.IGNORECASE)
