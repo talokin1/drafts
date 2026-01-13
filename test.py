ffd07641e21532be9d39192f6c66d2f3660d96b4
bovan494-wq <bovan494@gmail.com>
2026-01-13 09:38:21 +0200

    first commit

--- test.py
+++ test.py
@@ -1,19 +1,18 @@
-RE_REFUND_CASH = re.compile(
-    r"""
-    відшк\w* .*?
-    видач\w* \s+ готівк\w*
-    """,
-    re.IGNORECASE | re.VERBOSE
-)
+if re.search(r"розр\w*", pp_text):
+    anchors = 0
 
+    if RE_COMMISSION.search(pp_text):
+        anchors += 1
+    if re.search(r"\bтт\b", pp_text):
+        anchors += 1
+    if re.search(r"реєстр", pp_text):
+        anchors += 1
+    if RE_CMPS_MERCHANT.search(pp_text):
+        anchors += 1
 
-if RE_REFUND_CASH.search(pp_text) and (
-    RE_CMPS_MERCHANT.search(pp_text)
-    or RE_ADDRESS_HINT.search(pp_text)
-):
-    return pd.Series({
-        "is_acquiring": True,
-        "acq_reason": "refund_merchant_cash",
-        "acq_score": 3,
-    })
-
+    if anchors >= 2:
+        return pd.Series({
+            "is_acquiring": True,
+            "acq_reason": "acq_settlement",
+            "acq_score": 2 + anchors,
+        })
