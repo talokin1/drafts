d41089fe453c5ba4747a067b7c46438f86245d9a
bovan494-wq <bovan494@gmail.com>
2026-01-13 14:57:27 +0200

    first commit

--- test.py
+++ test.py
@@ -1,8 +1,4 @@
-df.loc[mask, "acq_reason"] = (
-    m_cp_inet.map({True: "cp_internet_acquiring", False: ""})
-    + m_cp_keyword.map({True: "|cp_acquiring_keyword", False: ""})
-).str.strip("|")
-
-
-pp_norm = pp.map(normalize_ua)
-m_refund_strong = pp_norm.str.contains(RE_REFUND_STRONG)
+def process_batch(df: pd.DataFrame) -> pd.DataFrame:
+    detected = df.apply(detect_acquiring, axis=1)
+    out = pd.concat([df.reset_index(drop=True), detected.reset_index(drop=True)], axis=1)
+    return out
