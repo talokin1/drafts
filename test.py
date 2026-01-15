65a27861a7f1d1dfc8ae04fe84da4a881c71f068
bovan494-wq <bovan494@gmail.com>
2026-01-15 12:50:33 +0200

    first commit

--- test.py
+++ test.py
@@ -1,35 +1,29 @@
-opf_ref = (
-    opf_df[["FIRM_OPFCD", "FIRM_OPFNM"]]
-    .dropna()
-    .drop_duplicates()
+a_side = (
+    trxs
+    .loc[trxs["CONTRAGENTAIDENTIFYCODE"].isin(client_ids),
+         ["CONTRAGENTAIDENTIFYCODE", "BANKAID"]]
     .rename(columns={
-        "FIRM_OPFCD": "OPF_CODE",
-        "FIRM_OPFNM": "OPF_NAME"
+        "CONTRAGENTAIDENTIFYCODE": "IDENTIFYCODE",
+        "BANKAID": "BANKID"
     })
 )
 
-opf_ref["OPF_NAME_NORM"] = (
-    opf_ref["OPF_NAME"]
-    .str.lower()
-    .str.replace(r"\s+", " ", regex=True)
-    .str.strip()
+b_side = (
+    trxs
+    .loc[trxs["CONTRAGENTBIDENTIFYCODE"].isin(client_ids),
+         ["CONTRAGENTBIDENTIFYCODE", "BANKBID"]]
+    .rename(columns={
+        "CONTRAGENTBIDENTIFYCODE": "IDENTIFYCODE",
+        "BANKBID": "BANKID"
+    })
 )
 
 
-df["FULL_NAME_NORM"] = (
-    df["Повна назва"]
-    .astype(str)
-    .str.lower()
-    .str.replace(r"[«»\"']", "", regex=True)
-    .str.replace(r"\s+", " ", regex=True)
-    .str.strip()
-)
-
-def extract_opf(full_name_norm, opf_ref):
-    for _, r in opf_ref.iterrows():
-        if full_name_norm.startswith(r["OPF_NAME_NORM"]):
-            return r["OPF_CODE"], r["OPF_NAME"]
-    return None, None
-df[["OPF_CODE", "OPF_NAME"]] = df["FULL_NAME_NORM"].apply(
-    lambda x: pd.Series(extract_opf(x, opf_ref))
+client_banks = (
+    pd.concat([a_side, b_side], ignore_index=True)
+    .dropna(subset=["BANKID"])
+    .groupby("IDENTIFYCODE", as_index=False)
+    .agg(
+        BANKS=("BANKID", lambda x: sorted(set(x)))
+    )
 )
