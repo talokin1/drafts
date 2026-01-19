69f1a13584cbb02bd3042b125a65706a44e4c5b0
bovan494-wq <bovan494@gmail.com>
2026-01-19 10:22:16 +0200

    first commit

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
-mask = y_test_cls == 1
+# Відбираємо тільки тих, у кого більше 0, але менше 100 грн (можна змінити на 200)
+small_positive = df[(df['CURR_ACC'] > 0) & (df['CURR_ACC'] < 100)]['CURR_ACC']
 
-mae_vip = mean_absolute_error(
-    np.expm1(y_test_log[mask]),
-    final_preds[mask]
-)
+print(f"Кількість клієнтів у зоні 0-100 грн: {len(small_positive)}")
+print(small_positive.describe(percentiles=[0.1, 0.25, 0.5, 0.75, 0.9]))
 
-r2_vip = r2_score(
-    np.expm1(y_test_log[mask]),
-    final_preds[mask]
-)
+import matplotlib.pyplot as plt
+plt.figure(figsize=(10, 4))
+plt.hist(small_positive, bins=50, edgecolor='black')
+plt.title("Розподіл залишків від 0 до 100 грн")
+plt.xlabel("Сума на рахунку")
+plt.ylabel("Кількість клієнтів")
+plt.show()
