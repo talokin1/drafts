79d542ce58c73699302cd084abc4258e31440c67
bovan494-wq <bovan494@gmail.com>
2026-01-20 17:35:34 +0200

    first commit

--- test.py
+++ test.py
@@ -1,17 +1,23 @@
-y_val_raw   = np.expm1(y_val)      # true грн
-y_pred_raw  = np.expm1(y_pred)     # pred грн
+target_clusters = [
+    "CEO",
+    "FORBES NEXT",
+    "Forbes 100",
+    "Gray Cardinals"
+]
 
-mae_uah = mean_absolute_error(y_val_raw, y_pred_raw)
-print("MAE (UAH):", mae_uah)
+df_filt = df[df["Кластер"].isin(target_clusters)]
 
 
-mae_zero_uah = mean_absolute_error(y_val_raw, np.zeros_like(y_val_raw))
 
-med_log = np.median(y_train)
-med_uah = np.expm1(med_log)
-pred_med_uah = np.full_like(y_val_raw, med_uah)
+result = (
+    df_filt
+    .groupby("Кластер")
+    .agg(
+        clients_cnt=("CONTRAGENTID", "nunique"),
+        avg_total_score=("TOTAL SCORE", "mean")
+    )
+    .reset_index()
+)
 
-mae_med_uah = mean_absolute_error(y_val_raw, pred_med_uah)
 
-print("Baseline ZERO MAE (UAH):", mae_zero_uah)
-print("Baseline MEDIAN MAE (UAH):", mae_med_uah)
+result["avg_total_score"] = result["avg_total_score"].round(2)
