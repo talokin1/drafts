6bf288dfc416e3d45ed5fba21256eb89e78cd945
bovan494-wq <bovan494@gmail.com>
2026-01-21 16:30:08 +0200

    first commit

--- test.py
+++ test.py
@@ -1,53 +1,24 @@
-cluster_block_summary = (
-    final_summary
-    .groupby("Кластер", as_index=False)
-    .agg(
-        clients_cnt = ("CONTRAGENTID", "count"),
-        avg_delta_total = ("DELTA_TOTAL_SCORE", "mean"),
-        avg_delta_business = ("DELTA_BUSINESS_SCORE", "mean"),
-        avg_delta_portfolio = ("DELTA_PORTFOLIO_SCORE", "mean"),
-        avg_delta_daily = ("DELTA_DAILY_SCORE", "mean"),
-    )
+FEATURE_MAP = {}
+FEATURE_MAP.update(BUSINESS_MAP)
+FEATURE_MAP.update(PORTFOLIO_MAP)
+FEATURE_MAP.update(DAILY_MAP)
+
+import re
+
+VALID_FEATURE_RE = re.compile(r'^(BUSINESS_B\d+|PORTFOLIO_P\d+|DAILY_D\d+)$')
+
+cluster_feature_impact = cluster_feature_impact[
+    cluster_feature_impact['FEATURE'].str.match(VALID_FEATURE_RE)
+].copy()
+
+def decode_feature(feature, mapping):
+    return mapping.get(feature, feature)
+
+cluster_feature_impact['FEATURE_TXT'] = cluster_feature_impact['FEATURE'].apply(
+    lambda x: decode_feature(x, FEATURE_MAP)
 )
-
-for col in ["business", "portfolio", "daily"]:
-    cluster_block_summary[f"share_{col}"] = (
-        cluster_block_summary[f"avg_delta_{col}"]
-        / cluster_block_summary["avg_delta_total"]
-    )
-
-
-cluster_main_driver = (
-    final_summary
-    .groupby(["Кластер", "MAIN_GROWTH_DRIVER"])
-    .size()
-    .reset_index(name="clients_cnt")
-)
-cluster_main_driver["share"] = (
-    cluster_main_driver
-    .groupby("Кластер")["clients_cnt"]
-    .transform(lambda x: x / x.sum())
-)
-
-
-def explode_drivers(df, cluster_col, drivers_col, block_name):
-    tmp = (
-        df[[cluster_col, drivers_col]]
-        .dropna()
-        .explode(drivers_col)
-    )
-    tmp["BLOCK"] = block_name
-    tmp.rename(columns={drivers_col: "FEATURE"}, inplace=True)
-    return tmp
-drivers_long = pd.concat([
-    explode_drivers(final_summary, "Кластер", "TOP_BUSINESS_DRIVERS", "BUSINESS"),
-    explode_drivers(final_summary, "Кластер", "TOP_PORTFOLIO_DRIVERS", "PORTFOLIO"),
-    explode_drivers(final_summary, "Кластер", "TOP_DAILY_DRIVERS", "DAILY"),
-])
 cluster_feature_impact = (
-    drivers_long
-    .groupby(["Кластер", "BLOCK", "FEATURE"])
-    .size()
-    .reset_index(name="cnt")
-    .sort_values(["Кластер", "BLOCK", "cnt"], ascending=[True, True, False])
+    cluster_feature_impact
+    .sort_values(['Кластер','cnt'], ascending=[True, False])
+    [['Кластер','BLOCK','FEATURE','FEATURE_TXT','cnt']]
 )
