2a5ba0bd963a3614da73b17b441db746dc398c71
bovan494-wq <bovan494@gmail.com>
2026-01-21 11:47:35 +0200

    first commit

--- test.py
+++ test.py
@@ -1,23 +1,50 @@
-target_clusters = [
-    "CEO",
-    "FORBES NEXT",
-    "Forbes 100",
-    "Gray Cardinals"
-]
-
-df_filt = df[df["Кластер"].isin(target_clusters)]
-
-
-
-result = (
-    df_filt
-    .groupby("Кластер")
-    .agg(
-        clients_cnt=("CONTRAGENTID", "nunique"),
-        avg_total_score=("TOTAL SCORE", "mean")
+cmp = (
+    df_nov[['CONTRAGENTID','TOTAL_SCORE',
+            'BUSINESS_SCORE','PORTFOLIO_SCORE','DAILY_SCORE']]
+    .merge(
+        df_mar[['CONTRAGENTID','TOTAL_SCORE',
+                'BUSINESS_SCORE','PORTFOLIO_SCORE','DAILY_SCORE']],
+        on='CONTRAGENTID',
+        suffixes=('_NOV','_MAR')
     )
-    .reset_index()
 )
 
+for col in ['TOTAL_SCORE','BUSINESS_SCORE','PORTFOLIO_SCORE','DAILY_SCORE']:
+    cmp[f'DELTA_{col}'] = cmp[f'{col}_MAR'] - cmp[f'{col}_NOV']
 
-result["avg_total_score"] = result["avg_total_score"].round(2)
+
+def growth_driver(row):
+    deltas = {
+        'BUSINESS'  : row['DELTA_BUSINESS_SCORE'],
+        'PORTFOLIO' : row['DELTA_PORTFOLIO_SCORE'],
+        'DAILY'     : row['DELTA_DAILY_SCORE'],
+    }
+    return max(deltas, key=deltas.get)
+cmp['MAIN_GROWTH_DRIVER'] = cmp.apply(growth_driver, axis=1)
+
+
+
+daily_delta = (
+    df_mar[['CONTRAGENTID'] + DAILY_COLS]
+    .set_index('CONTRAGENTID')
+    - df_nov[['CONTRAGENTID'] + DAILY_COLS]
+      .set_index('CONTRAGENTID')
+).reset_index()
+
+def top_daily_drivers(row, n=3):
+    s = row[DAILY_COLS]
+    return list(s.sort_values(ascending=False).head(n).index)
+
+daily_delta['TOP_DAILY_DRIVERS'] = daily_delta.apply(top_daily_drivers, axis=1)
+
+
+
+
+final = (
+    cmp.merge(
+        daily_delta[['CONTRAGENTID','TOP_DAILY_DRIVERS']],
+        on='CONTRAGENTID',
+        how='left'
+    )
+    .sort_values('DELTA_TOTAL_SCORE', ascending=False)
+)
