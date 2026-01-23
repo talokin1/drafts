2f09cd92f5a03f6aebc7eece7ecb15b1614d8fb3
bovan494-wq <bovan494@gmail.com>
2026-01-23 15:22:35 +0200

    first commit

--- test.py
+++ test.py
@@ -1,41 +1,7 @@
-# Список нових колонок, які ми додаємо з ubki
-new_kved_cols = [
-    'KVED', 'KVED_DESCR',
-    'KVED_2', 'KVED_2_DESCR',
-    'KVED_3', 'KVED_3_DESCR',
-    'KVED_4', 'KVED_4_DESCR',
-    'KVED_5', 'KVED_5_DESCR'
-]
+# 1. Визначаємо, які з нових колонок вже є в таблиці, і видаляємо їх, 
+# щоб merge не створив дублікатів типу KVED_x / KVED_y
+cols_overlap = [c for c in new_kved_cols if c in fin_ind.columns]
+fin_ind = fin_ind.drop(columns=cols_overlap)
 
-# 1. Знаходимо місце вставки (індекс старої колонки FIRM_KVED)
-# Це потрібно зробити ДО мерджу та видалення
-if 'FIRM_KVED' in fin_ind.columns:
-    insert_loc = fin_ind.columns.get_loc('FIRM_KVED')
-elif 'FIRM_KVEDNM' in fin_ind.columns:
-    insert_loc = fin_ind.columns.get_loc('FIRM_KVEDNM')
-else:
-    insert_loc = len(fin_ind.columns)
-
-# Зберігаємо список "базових" колонок (все крім тих, що будемо замінювати)
-cols_to_remove = ['FIRM_KVED', 'FIRM_KVEDNM']
-base_cols = [c for c in fin_ind.columns if c not in cols_to_remove]
-
-# 2. Виконуємо злиття (Left Join)
-# fin_ind залишається головним, підтягуємо дані з ubki
+# 2. Тепер ваш код спрацює коректно, бо конфлікту імен не буде
 fin_ind = fin_ind.merge(ubki[['IDENTIFYCODE'] + new_kved_cols], on='IDENTIFYCODE', how='left')
-
-# 3. КЛЮЧОВИЙ МОМЕНТ: Зберігаємо старі дані для рядків, де не знайшлося відповідності в ubki
-# Якщо в новій колонці KVED пусто (NaN), беремо значення зі старої FIRM_KVED
-if 'FIRM_KVED' in fin_ind.columns:
-    fin_ind['KVED'] = fin_ind['KVED'].fillna(fin_ind['FIRM_KVED'])
-
-# Аналогічно для опису: якщо KVED_DESCR пусто, беремо з FIRM_KVEDNM
-if 'FIRM_KVEDNM' in fin_ind.columns:
-    fin_ind['KVED_DESCR'] = fin_ind['KVED_DESCR'].fillna(fin_ind['FIRM_KVEDNM'])
-
-# 4. Формуємо фінальний порядок колонок
-# Розрізаємо список базових колонок у точці insert_loc і вставляємо туди блок нових колонок
-# Оскільки ми використовуємо base_cols (без старих FIRM_...), старі колонки автоматично зникнуть з фінального датафрейму
-final_cols = base_cols[:insert_loc] + new_kved_cols + base_cols[insert_loc:]
-
-fin_ind = fin_ind[final_cols]
