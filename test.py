e13d878a197b1bcd49c96270b2e86017a37cc29c
bovan494-wq <bovan494@gmail.com>
2026-01-23 14:51:30 +0200

    first commit

--- test.py
+++ test.py
@@ -1,16 +1,16 @@
-# 1. Виконуємо злиття (merge)
-# Використовуємо 'left', щоб зберегти всі рядки з основного датафрейму fin_ind
-# Також явно вказуємо колонки з ubki, які нам потрібні, плюс ключ для злиття
-cols_to_merge = ['IDENTIFYCODE', 'SECTION_CODE', 'SECTION_NAME', 'DIVISION_NAME', 'GROUP_NAME']
+# Список усіх колонок КВЕДів, які ми бачимо на скріншотах
+kved_cols = [
+    'KVED', 'KVED_DESCR',
+    'KVED_2', 'KVED_2_DESCR',
+    'KVED_3', 'KVED_3_DESCR',
+    'KVED_4', 'KVED_4_DESCR',
+    'KVED_5', 'KVED_5_DESCR'
+]
 
-fin_ind = fin_ind.merge(ubki[cols_to_merge], on='IDENTIFYCODE', how='left')
+# 1. Видаляємо старі колонки, які треба замінити
+# errors='ignore' дозволяє уникнути помилки, якщо колонок вже немає
+fin_ind = fin_ind.drop(columns=['FIRM_KVED', 'FIRM_KVEDNM'], errors='ignore')
 
-# 2. Перевпорядковуємо колонки, щоб нові дані були на початку
-# Список нових колонок у бажаному порядку
-new_cols_order = ['SECTION_CODE', 'SECTION_NAME', 'DIVISION_NAME', 'GROUP_NAME']
-
-# Решта колонок, які вже були в fin_ind (виключаючи ті, що ми щойно додали в список вище)
-remaining_cols = [col for col in fin_ind.columns if col not in new_cols_order]
-
-# Фінальне перевпорядкування
-fin_ind = fin_ind[new_cols_order + remaining_cols]
+# 2. Виконуємо злиття
+# Беремо з ubki тільки ключ (IDENTIFYCODE) та потрібні колонки
+fin_ind = fin_ind.merge(ubki[['IDENTIFYCODE'] + kved_cols], on='IDENTIFYCODE', how='left')
