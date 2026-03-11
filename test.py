c66a851aa49c5eceddc53ddc2c5ff239111f5282
bovan494-wq <bovan494@gmail.com>
2026-03-11 15:42:20 +0200

    Num changes

--- test.py
+++ test.py
@@ -1,21 +1,17 @@
-# 1. Читаємо всі файли та створюємо список пар ID - Банк
+# 1. Збираємо всі пари Клієнт-Банк
 df_p_list = []
 for file in all_files:
     df_temp = pd.read_excel(file)
     bank_name = os.path.basename(file).replace('_clients.xlsx', '').replace('.xlsx', '')
-    
-    # Вибираємо тільки потрібну колонку
     temp_subset = df_temp[['IDENTIFYCODE']].copy()
     temp_subset['Source_Bank'] = bank_name
     df_p_list.append(temp_subset)
 
-# Об'єднуємо все в одну "довгу" таблицю
-df_all_potential = pd.concat(df_p_list, ignore_index=True)
+df_mapping = pd.concat(df_p_list, ignore_index=True).drop_duplicates()
 
-# ГРУПУВАННЯ (щоб уникнути твого кейсу з дублями)
-# Для кожного ID збираємо всі унікальні банки в один рядок
-df_p_unique = df_all_potential.groupby('IDENTIFYCODE')['Source_Bank'].apply(
-    lambda x: ', '.join(sorted(x.unique()))
-).reset_index()
+# 2. Fact_Income тепер буде містити Source_Bank
+# Це дозволить Power BI рахувати гроші по кожному банку окремо
+df_facts_with_banks = pd.merge(df_facts, df_mapping, on='IDENTIFYCODE', how='left')
 
-# Тепер у нас є унікальна множина Dim_Clients
+# 3. Зберігаємо
+df_facts_with_banks.to_csv('Fact_Income.csv', index=False)
