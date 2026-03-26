cae6e49f62df8295b0444681b85a577e9a906b99
bovan494-wq <bovan494@gmail.com>
2026-03-26 14:55:40 +0200

    Num changes

--- test.py
+++ test.py
@@ -1,14 +1,19 @@
-class TransactionsModel:
+class FXModel:
     def __init__(self, model, bucket_medians, cat_cols, cat_values=None):
         self.model = model
-        self.bucket_medians = bucket_medians
+        self.bucket_medians = bucket_medians  # тут ЗБЕРІГАЄМО log-медіани
         self.cat_cols = cat_cols
         self.cat_values = cat_values
+
+        # одразу підготуємо medians в original scale
+        self.medians_orig = np.array([
+            np.expm1(self.bucket_medians[i]) for i in range(len(self.bucket_medians))
+        ])
 
     def predict(self, X):
         X = X.copy()
 
-        # категоріальні фічі
+        # категоріальні
         for c in self.cat_cols:
             if self.cat_values:
                 X[c] = pd.Categorical(X[c], categories=self.cat_values[c])
@@ -17,19 +22,12 @@ class TransactionsModel:
 
         probs = self.model.predict_proba(X)
 
-        # E[y_log]
-        medians = np.array([
-            self.bucket_medians[i] for i in range(len(self.bucket_medians))
-        ])
-        y_log_pred = probs @ medians
-
-        # повертаємо в original scale
-        y_pred = np.expm1(y_log_pred)
+        # E[y] вже в original scale
+        y_pred = probs @ self.medians_orig
         y_pred = np.clip(y_pred, 0, None)
 
         return y_pred
     
-
 cat_values = {
     c: X_train[c].cat.categories
     for c in cat_cols
@@ -37,11 +35,11 @@ cat_values = {
 
 import joblib
 
-wrapped_model = TransactionsModel(
+wrapped_model = FXModel(
     model=clf_model,
-    bucket_medians=bucket_medians.to_dict(),
+    bucket_medians=bucket_medians.to_dict(),  # лог-медіани
     cat_cols=cat_cols,
     cat_values=cat_values
 )
 
-joblib.dump(wrapped_model, "transactions_model.pkl")
+joblib.dump(wrapped_model, "fx_model.pkl")
