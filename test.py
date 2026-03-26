81d5fc2bcb399d3e71abeec00fb193600dc8c78f
bovan494-wq <bovan494@gmail.com>
2026-03-26 14:37:32 +0200

    Num changes

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-class AssetsBucketModel:
+class TransactionsModel:
     def __init__(self, model, bucket_medians, cat_cols, cat_values=None):
         self.model = model
         self.bucket_medians = bucket_medians
@@ -17,21 +17,31 @@ class AssetsBucketModel:
 
         probs = self.model.predict_proba(X)
 
-        # E[Y] через ймовірності
-        y_pred = np.zeros(len(X))
-        for i in range(len(self.bucket_medians)):
-            y_pred += probs[:, i] * self.bucket_medians[i]
+        # E[y_log]
+        medians = np.array([
+            self.bucket_medians[i] for i in range(len(self.bucket_medians))
+        ])
+        y_log_pred = probs @ medians
+
+        # повертаємо в original scale
+        y_pred = np.expm1(y_log_pred)
+        y_pred = np.clip(y_pred, 0, None)
 
         return y_pred
     
 
+cat_values = {
+    c: X_train[c].cat.categories
+    for c in cat_cols
+}
+
 import joblib
 
-wrapped_model = AssetsBucketModel(
+wrapped_model = TransactionsModel(
     model=clf_model,
-    bucket_medians=bucket_medians,
+    bucket_medians=bucket_medians.to_dict(),
     cat_cols=cat_cols,
     cat_values=cat_values
 )
 
-joblib.dump(wrapped_model, "assets_bucket_model.pkl")
+joblib.dump(wrapped_model, "transactions_model.pkl")
