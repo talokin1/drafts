995f71cb22ebd2be2f01f9bc160333496551bc80
bovan494-wq <bovan494@gmail.com>
2026-04-07 17:46:37 +0300

    Num changes

--- test.py
+++ test.py
@@ -1,17 +1,19 @@
+import numpy as np
+import pandas as pd
+
 def normalize_kved(x):
     if pd.isna(x):
         return np.nan
     
     x = str(x).strip()
-    
+    x = x.replace(',', '.')  # 🔥 ключовий момент
+
     if '.' in x:
         left, right = x.split('.')
     else:
         left, right = x, ''
     
-    left = left.zfill(2)      # 1 → 01
-    right = right.ljust(2, '0')  # 2 → 20
+    left = left.zfill(2)
+    right = right.ljust(2, '0')
     
     return f"{left}.{right[:2]}"
-
-clients['FIRM_KVED'] = clients['FIRM_KVED'].apply(normalize_kved)
