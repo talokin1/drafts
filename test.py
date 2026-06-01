58ffda51664f0da1cdb6dc4ba3c70eada577edfc
bovan494-wq <bovan494@gmail.com>
2026-06-01 16:01:03 +0300

    Drafts

--- test.py
+++ test.py
@@ -1,99 +1,26 @@
-model = HierarchicalSegmentModel(
-    cat_cols=cat_cols,
-    stage1_positive_boost=1.2,
-    stage2_hnwi_boost=1.3,
-    random_state=42
+rec = (
+    liabs[["IDENTIFYCODE", "PRIMARY"]]
+    .rename(columns={"PRIMARY": "p_liabs"})
+    .merge(
+        assets[["IDENTIFYCODE", "PRIMARY"]].rename(columns={"PRIMARY": "p_assets"}),
+        on="IDENTIFYCODE",
+        how="outer"
+    )
+    .merge(
+        fx[["IDENTIFYCODE", "PROB_TO_FX"]].rename(columns={"PROB_TO_FX": "p_fx"}),
+        on="IDENTIFYCODE",
+        how="outer"
+    )
 )
 
-model.fit(
-    X_train,
-    y_train,
-    X_valid=X_valid,
-    y_valid=y_valid
-)
+rec[["p_liabs", "p_assets", "p_fx"]] = rec[["p_liabs", "p_assets", "p_fx"]].fillna(0)
 
-valid_proba = model.predict_proba(X_valid)
+rec["recommended_product"] = rec[["p_liabs", "p_assets", "p_fx"]].idxmax(axis=1)
 
+rec["recommended_product"] = rec["recommended_product"].map({
+    "p_liabs": "Liabilities",
+    "p_assets": "Assets",
+    "p_fx": "FX"
+})
 
-
-
-
-threshold_results = []
-
-hnwi_grid = np.arange(0.10, 0.81, 0.025)
-prem_grid = np.arange(0.20, 0.91, 0.025)
-
-for hnwi_thr in hnwi_grid:
-    for prem_thr in prem_grid:
-        res = evaluate_thresholds(
-            y_true=y_valid,
-            proba=valid_proba,
-            hnwi_threshold=hnwi_thr,
-            prem_threshold=prem_thr
-        )
-
-        res["business_score_balanced"] = (
-            0.25 * res["hnwi_recall"] +
-            0.20 * res["hnwi_precision"] +
-            0.20 * res["prem_recall"] +
-            0.15 * res["mass_recall"] +
-            0.20 * res["macro_f1"]
-        )
-
-        res["business_score_discovery"] = (
-            0.45 * res["hnwi_recall"] +
-            0.25 * res["hnwi_precision"] +
-            0.15 * res["prem_recall"] +
-            0.10 * res["mass_recall"] +
-            0.05 * res["macro_f1"]
-        )
-
-        threshold_results.append(res)
-
-threshold_results_df = pd.DataFrame(threshold_results)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-balanced_candidates = threshold_results_df[
-    (threshold_results_df["hnwi_precision"] >= 0.12) &
-    (threshold_results_df["hnwi_recall"] >= 0.50) &
-    (threshold_results_df["mass_recall"] >= 0.70) &
-    (threshold_results_df["prem_recall"] >= 0.20)
-].copy()
-
-display(
-    balanced_candidates
-    .sort_values("business_score_balanced", ascending=False)
-    .head(20)
-)
-
-
-
-
-
-
-
-discovery_candidates = threshold_results_df[
-    (threshold_results_df["hnwi_recall"] >= 0.70) &
-    (threshold_results_df["hnwi_precision"] >= 0.10) &
-    (threshold_results_df["mass_recall"] >= 0.55)
-].copy()
-
-display(
-    discovery_candidates
-    .sort_values("business_score_discovery", ascending=False)
-    .head(20)
-)
+rec["recommendation_score"] = rec[["p_liabs", "p_assets", "p_fx"]].max(axis=1)
